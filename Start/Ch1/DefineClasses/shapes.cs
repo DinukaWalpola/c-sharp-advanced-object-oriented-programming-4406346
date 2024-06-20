372e8d6c32364ccae74b0eb00a0089d4a60ccd73
DinukaWalpola <91844262+DinukaWalpola@users.noreply.github.com>
2024-06-20 04:23:32 +0000

    Completed the DefineClasses task

--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,28 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
+public class Rectangle{
 
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle (int h, int w) {
+        height = h;
+        width = w;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
+    public Rectangle (int side) {
+        height = width = side;
+    }
 
 
     // TODO: Classes can define methods that return values
+    public int GetArea() {
+        return height * width;
+    }
 
 
     // TODO: member variables hold data
+    public int height;
+    public int width;
+}
